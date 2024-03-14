9e9ffd5dc5a7bb537008a5ab46d36ec4d32c04e5
Akira <37931@ma-web.nl>
2024-03-14 10:12:04 +0100

    variabelen

---
+++ m3prog_project/public/02/variabelen.php
@@ -0,0 +1,17 @@
+<?php
+
+  $auto2 = "Mercedes";
+  $_naam = "Akira";
+  //$auto's = "werkt niet"
+  //$auto/onderdelen = "werkt niet"
+  //$huis&tuin = "werkt niet"
+  $naw = "test";
+  //$22 = "werkt niet"
+  //$$auto = "werkt niet"
+  $kleurVanAuto = "blauw";
+  //$software-dev = "werkt niet"
+
+  echo $variabele = "blabla";
+  
+?>
+
