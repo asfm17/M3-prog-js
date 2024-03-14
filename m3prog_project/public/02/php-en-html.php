aa2f83e198ddd2c1533e502057dcdc7f0ea07067
Akira <37931@ma-web.nl>
2024-03-14 10:29:06 +0100

    shrek

---
+++ m3prog_project/public/02/php-en-html.php
@@ -0,0 +1,28 @@
+<?php
+
+  $titel = "Shrek";
+  $image = "https://i.seadn.io/gae/2hDpuTi-0AMKvoZJGd-yKWvK4tKdQr_kLIpB_qSeMau2TNGCNidAosMEvrEXFO9G6tmlFlPQplpwiqirgrIPWnCKMvElaYgI-HiVvXc?auto=format&dpr=1&w=1000";
+  $titel_afbeelding = "Shrek meme";
+  $auteur = "asfm17";
+  $mijnVariable = "Shrek"
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <main>
+     <?=$mijnVariable?> 
+   </main>
+   <img src="https://i.seadn.io/gae/2hDpuTi-0AMKvoZJGd-yKWvK4tKdQr_kLIpB_qSeMau2TNGCNidAosMEvrEXFO9G6tmlFlPQplpwiqirgrIPWnCKMvElaYgI-HiVvXc?auto=format&dpr=1&w=1000" alt="Shrek meme">
+   <section>
+    Auteur: <?=$auteur?>
+   </section>
+</body>
+</html>
